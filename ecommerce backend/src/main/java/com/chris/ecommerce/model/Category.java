dab5ac3a4dc8662506cc54afb77632518a0cdc48
Chris Gjuraj <cgjuraj@gmail.com>
2023-05-30 20:38:30 -0400

    categories added

--- ecommerce backend/src/main/java/com/chris/ecommerce/model/Category.java
+++ ecommerce backend/src/main/java/com/chris/ecommerce/model/Category.java
@@ -1,9 +1,20 @@
 package com.chris.ecommerce.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
 
 @Entity
 @Table(name = "category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    private String description;
+
+    private String imageUrl;
 }
